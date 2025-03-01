828b9bfa26e1a712a45eba12619caeb7041008e3
Mandar Jadhav <mjadhav19@outlook.com>
2025-03-02 00:12:05 +0530

    Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from '@vercel/analytics/next';
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider>
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 };
