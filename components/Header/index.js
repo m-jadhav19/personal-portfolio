450ecea7b51cf03f00dd16938c3ce9dbf0bda65b
Mandar Jadhav <jadhavmandar44@gmail.com>
2025-09-14 22:44:13 +0530

    Refactor Header component to replace Image elements with SVGs for theme toggling and menu functionality. Updated SVG fill colors to use 'currentColor' for better theming support. Enhanced styling for header elements to utilize dynamic color variables.

--- components/Header/index.js
+++ components/Header/index.js
@@ -24,16 +24,22 @@ const Header = ({handleWorkScroll, handleAboutScroll, isBlog}) => {
 
 		return (
 			<Button onClick={() => setTheme(currentTheme === 'dark' ? 'light' : 'dark')}>
-				<Image
-					className='h-6 transition-transform duration-300 transform'
+				<div
+					className='h-6 w-6 transition-transform duration-300 transform'
 					style={{
 						transform: currentTheme === 'dark' ? 'rotate(0deg)' : 'rotate(360deg)',
-					}}
-					src={`/images/${currentTheme === 'dark' ? 'moon.svg' : 'sun.svg'}`}
-					alt='Theme Switcher'
-					width={24}
-					height={24}
-				/>
+						color: 'var(--selected-color, #00cdac)'
+					}}>
+					{currentTheme === 'dark' ? (
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-full h-full">
+							<path fillRule="evenodd" d="M9.528 1.718a.75.75 0 0 1 .162.819A8.97 8.97 0 0 0 9 6a9 9 0 0 0 9 9 8.97 8.97 0 0 0 3.463-.69.75.75 0 0 1 .981.98 10.503 10.503 0 0 1-9.694 6.46c-5.799 0-10.5-4.7-10.5-10.5 0-4.368 2.667-8.112 6.46-9.694a.75.75 0 0 1 .818.162Z" clipRule="evenodd" />
+						</svg>
+					) : (
+						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-full h-full">
+							<path d="M12 2.25a.75.75 0 0 1 .75.75v2.25a.75.75 0 0 1-1.5 0V3a.75.75 0 0 1 .75-.75ZM7.5 12a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM18.894 6.166a.75.75 0 0 0-1.06-1.06l-1.591 1.59a.75.75 0 1 0 1.06 1.061l1.591-1.59ZM21.75 12a.75.75 0 0 1-.75.75h-2.25a.75.75 0 0 1 0-1.5H21a.75.75 0 0 1 .75.75ZM17.834 18.894a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 1 0-1.061 1.06l1.59 1.591ZM12 18a.75.75 0 0 1 .75.75V21a.75.75 0 0 1-1.5 0v-2.25A.75.75 0 0 1 12 18ZM7.758 17.303a.75.75 0 0 0-1.061-1.06l-1.591 1.59a.75.75 0 0 0 1.06 1.061l1.591-1.59ZM6 12a.75.75 0 0 1-.75.75H3a.75.75 0 0 1 0-1.5h2.25A.75.75 0 0 1 6 12ZM6.697 7.757a.75.75 0 0 0 1.06-1.06l-1.59-1.591a.75.75 0 0 0-1.061 1.06l1.59 1.591Z" />
+						</svg>
+					)}
+				</div>
 			</Button>
 		)
 	}
@@ -46,7 +52,8 @@ const Header = ({handleWorkScroll, handleAboutScroll, isBlog}) => {
 						<div className={`header-mobile header-extra-wide ${theme === 'dark' ? 'dark' : ''} flex items-center justify-between p-2 laptop:p-0 mx-auto`}>
 							<h1
 								onClick={() => router.push('/')}
-								className='logo-code liquid-glass-logo font-fira-code text-xl font-bold p-2 laptop:p-0 link cursor-pointer'>
+								className='logo-code liquid-glass-logo font-fira-code text-xl font-bold p-2 laptop:p-0 link cursor-pointer'
+								style={{ color: 'var(--selected-color, #00cdac)' }}>
 								&lt;/MJ&gt;
 							</h1>
 
@@ -54,16 +61,22 @@ const Header = ({handleWorkScroll, handleAboutScroll, isBlog}) => {
 								{data.darkMode && renderThemeChanger()}
 
 								<Popover.Button>
-									<Image
-										className='h-6 my-auto transition-transform duration-300 transform'
+									<div
+										className='h-6 w-6 my-auto transition-transform duration-300 transform'
 										style={{
 											transform: !open ? 'rotate(0deg)' : 'rotate(360deg)',
-										}}
-										src={`/images/${!open ? 'menu.svg' : 'cancel.svg'}`}
-										alt='Menu'
-										width={24}
-										height={24}
-									/>
+											color: 'var(--selected-color, #00cdac)'
+										}}>
+										{!open ? (
+											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-full h-full">
+												<path fillRule="evenodd" d="M3 5.25a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 5.25Zm0 4.5A.75.75 0 0 1 3.75 9h16.5a.75.75 0 0 1 0 1.5H3.75A.75.75 0 0 1 3 9.75Zm0 4.5a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Zm0 4.5a.75.75 0 0 1 .75-.75h16.5a.75.75 0 0 1 0 1.5H3.75a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
+											</svg>
+										) : (
+											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-full h-full">
+												<path fillRule="evenodd" d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
+											</svg>
+										)}
+									</div>
 								</Popover.Button>
 							</div>
 						</div>
@@ -103,7 +116,8 @@ const Header = ({handleWorkScroll, handleAboutScroll, isBlog}) => {
 			<div className={`header-desktop header-extra-wide ${theme === 'dark' ? 'dark' : ''} mt-10 hidden flex-row items-center justify-between sticky top-4 z-10 tablet:flex mx-auto px-12`}>
 				<h1
 					onClick={() => router.push('/')}
-					className='logo-code liquid-glass-logo font-fira-code text-2xl font-bold cursor-pointer ml-4 mob:p-2 laptop:p-0'>
+					className='logo-code liquid-glass-logo font-fira-code text-2xl font-bold cursor-pointer ml-4 mob:p-2 laptop:p-0'
+					style={{ color: 'var(--selected-color, #00cdac)' }}>
 					&lt;/MJ&gt;
 				</h1>
 				{!isBlog ? (
